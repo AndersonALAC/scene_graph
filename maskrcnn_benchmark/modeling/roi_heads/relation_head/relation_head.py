e6e5cf931728cf98fcef386901554ea26b8349fe
ALAC <408005134@qq.com>
2022-05-15 02:10:49 +0800

    relation head

--- maskrcnn_benchmark/modeling/roi_heads/relation_head/relation_head.py
+++ maskrcnn_benchmark/modeling/roi_heads/relation_head/relation_head.py
@@ -85,7 +85,7 @@ class ROIRelationHead(torch.nn.Module):
         refine_logits, relation_logits, add_losses = self.predictor(proposals, rel_pair_idxs, rel_labels, rel_binarys, roi_features, union_features, logger)
 
         if self.cfg.MODEL.BALANCED_NORM:
-            relation_probs_norm, labeling_prob, rel_labels_one_hot_count = self.balanced_norm((relation_logits, refine_logits), rel_labels)
+            relation_probs_norm, labeling_prob, rel_labels_one_hot_count = self.balanced_norm(relation_logits, rel_labels)
         else:
             relation_probs_norm = labeling_prob = None
 
